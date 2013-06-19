9be2e129031356780576029476c36405d0680e3e
Violeta Rosales <ellaes@violetarosales.com>
2013-06-19 13:25:33 -0500

    Usando un poco le lo de Wiki

--- am-web/js/scripts.js
+++ am-web/js/scripts.js
@@ -1,4 +1,6 @@
 $(function() {
+
+	// slider
 	$('#slider').slidesjs({
 		width: 1480,
 		height: 380,
@@ -21,4 +23,45 @@ $(function() {
 			}
 		}
 	});
+
+	//submenu
+	$('#main-nav > ul li a').click(function() {
+		
+		if($(this).hasClass('activo')){
+			$(this).removeClass("activo");
+		}
+		else {
+			$(this).addClass("activo");
+		}
+		// fix brinco en menu
+		$("#main-nav").css("position", "absolute")
+	});
+
+	// fix brinco en menu
+	$('.close-menu').click(function() {
+		
+		$("#main-nav").css("position", "fixed")
+	});
+
+	// para el paginador ajax, un demo
+	$('.load_more').click(function() {
+		$('.cargados').append('<section class="lista"><article><a href="articulo.html"><img src="http://placehold.it/100x100" alt="WikiMéxico"><div class="texto"><h2>El misterio de los anasazi </h2><p class="meta">Por: Claudia Espinoza<br>Fecha: 21/05/2013</p></div></a></article></section><section class="lista"><article><a href="articulo.html"><img src="http://placehold.it/100x100" alt="WikiMéxico"><div class="texto"><h2>El misterio de los anasazi </h2><p class="meta">Por: Claudia Espinoza<br>Fecha: 21/05/2013</p></div></a></article></section><section class="lista"><article><a href="articulo.html"><img src="http://placehold.it/100x100" alt="WikiMéxico"><div class="texto"><h2>El misterio de los anasazi </h2><p class="meta">Por: Claudia Espinoza<br>Fecha: 21/05/2013</p></div></a></article></section><section class="lista"><article><a href="articulo.html"><img src="http://placehold.it/100x100" alt="WikiMéxico"><div class="texto"><h2>El misterio de los anasazi </h2><p class="meta">Por: Claudia Espinoza<br>Fecha: 21/05/2013</p></div></a></article></section><section class="lista"><article><a href="articulo.html"><img src="http://placehold.it/100x100" alt="WikiMéxico"><div class="texto"><h2>El misterio de los anasazi </h2><p class="meta">Por: Claudia Espinoza<br>Fecha: 21/05/2013</p></div></a></article></section>');
+		alto = $(".page-wrap").height()
+	$("#main-nav").css("min-height", alto)
+	});
+
+});
+
+$(window).load(function(){
+	var alto;
+	// alto minimo
+	alto = $(".page-wrap").height()
+	$("#main-nav").css("min-height", alto)
+});
+
+$(window).resize(function() {
+	var alto;
+	// alto minimo
+	alto = $(".page-wrap").height()
+	$("#main-nav").css("min-height", alto)
 });
